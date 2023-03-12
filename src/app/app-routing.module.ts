9b063c666bfb7b72ff83f898e704d8ca013a164e
Luis Carlos <roxisxprol@gmail.com>
2023-03-11 21:31:06 -0300

    Create App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
+import { ContatosComponent } from './components/main/contatos/contatos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoriaComponent } from './components/main/historia/historia.component';
+import { MissoesComponent } from './components/main/missoes/missoes.component';
+import { EventosComponent } from './components/main/eventos/eventos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HistoriaComponent
+  },
+  {
+    path: 'historia',
+    component: HistoriaComponent
+  },
+  {
+    path: 'missoes',
+    component: MissoesComponent
+  },
+  {
+    path: 'eventos',
+    component: EventosComponent
+  },
+  {
+    path: 'contatos',
+    component: ContatosComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import { MaterialModule } from './modules/material/material.module';
+import { HistoriaComponent } from './components/main/historia/historia.component';
+import { MissoesComponent } from './components/main/missoes/missoes.component';
+import { EventosComponent } from './components/main/eventos/eventos.component';
+import { ContatosComponent } from './components/main/contatos/contatos.component';
+import { MatCarouselModule } from '@magloft/material-carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HistoriaComponent,
+    MissoesComponent,
+    EventosComponent,
+    ContatosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+     MatCarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
