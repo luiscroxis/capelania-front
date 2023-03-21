ea666b50e98519ac50d7b2cd61d6b5cee040017c
Luis Carlos <luiscroxis@gmail.com>
2023-03-20 21:00:58 -0300

    Create component Contato

--- src/app/components/main/contatos/contatos.component.ts
+++ src/app/components/main/contatos/contatos.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ContatosComponent {
 
+  contato: string = '(11)98936-8808'
+  userLink: string = '@CAPELANIASALTOAMIGOSDOBEM'
+  email: string = 'capelaniasaltoamigosdobem@gmail.com'
+  pix: string = 'pix: 11974010947'
+
 }
